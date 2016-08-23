9741d288fdad53f2d7b5955c51c7edb7321c2c34
srinivasaraokandregula <seenukandregula@gmail.com>
2016-08-23 13:50:37 +0530

    react first commit

---
+++ js/components/Component1.js
@@ -0,0 +1,24 @@
+var React=require('react');
+var ReactDom=require('react-dom');
+var Component2=require('./Component2');
+
+var Component1=React.createClass({
+  render:function(){
+        // console.log("daaaaa"+JSON.stringify(this.props.ldata);
+          var llabel=this.props.allLabelsData.map(function(e,i){
+            return(
+              <div key={i}>
+                <Component2 key={i} id={e.id} name={e.name}></Component2>
+              </div>
+            );
+          });
+          return(
+            <div>
+            {llabel}
+            </div>
+          );
+
+
+  }
+});
+module.exports=Component1
---
+++ js/components/Component2.js
@@ -0,0 +1,10 @@
+var React=require('react');
+var Component2=React.createClass({
+render:function(){
+  return(
+        <a id={this.props.name} href="#">{this.props.name}</a>
+  
+  );
+}
+});
+module.exports=Component2
